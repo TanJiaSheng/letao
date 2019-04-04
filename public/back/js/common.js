f85c9849ddb8592b5cec2b46304e1f8075288a6a
TJS-95 <TJS_95@163.com>
2019-04-04 11:59:12 +0800

    首页-公共功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -24,3 +24,67 @@ $(document).ajaxStop(function() {
         NProgress.done();
     },500);
 });
+
+// 登录拦截功能，登录界面不需要校验，不用登录直接访问
+// 前后端分离了，前段是不知道该用户是否登录了，但后台知道
+// 发送 ajax 请求，查询用户状态即可
+// (1) 用户已登录，啥都不做，让用户继续访问
+// (2) 用户未登录，拦截到登录页
+
+if(location.href.indexOf("login.html") === -1){
+    // 地址栏中没有 login.html，说明不是登录页，需要进行登录拦截
+    $.ajax({
+        type: "get",
+        url: "/employee/checkRootLogin",
+        dataType: "json",
+        success: function(info) {
+            //console.log(info);
+            if(info.success) {
+                //已登录，让用户继续访问
+            }
+            if(info.error === 400) {
+                //未登录，拦截到登录页
+                location.href = "login.html";
+            }
+        }
+    });
+}
+
+
+$(function() {
+   //1. 分类管理的切换功能
+    $(".category").on("click", function() {
+        //切换child 的显示隐藏
+        $('.nav .child').stop().slideToggle();
+    });
+
+   //2.左侧侧边栏切换功能
+    $('.icon_menu').on("click", function() {
+        $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_main').toggleClass('hidemenu');
+        $('.lt_topbar').toggleClass('hidemenu');
+    });
+
+   //3.点击topbar退出按钮，弹出模态框
+    $('.icon_logout').on("click", function() {
+        //显示模态框，modal("show")
+        $('#logoutModal').modal('show');
+    });
+
+    //4. 点击模态框的退出按钮，实现推迟功能
+    $('#logoutBtn').on("click", function() {
+        //发送ajax请求，进行退出
+        $.ajax({
+            type: "get",
+            url: "/employee/employeeLogout",
+            dataType: "json",
+            success: function(info) {
+                //console.log(info);
+                if(info.success) {
+                    //退出成功,跳转到登录页
+                    location.href = "login.html";
+                }
+            }
+        });
+    });
+});
