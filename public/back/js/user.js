9daacd71d650a8e0af9f65a46956b6bf59f13220
TJS-95 <TJS_95@163.com>
2019-04-09 17:50:26 +0800

    用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,6 +1,8 @@
 $(function () {
-    var currentPage = 1;
-    var pageSize = 5;
+    var currentPage = 1;    //当前页
+    var pageSize = 5;   //每页多少条
+    var currentId; //当前用户id
+    var isDelete;
     //1. 一进入页面，发送ajax请求，获取用户列表数据，通过模板引擎渲染
     render();
     function render() {
@@ -40,4 +42,37 @@ $(function () {
             }
         });
     }
+
+    //2. 点击启用禁用按钮，显示模态框，通过事件委托绑定事件
+    $('tbody').on("click", ".btn", function(){
+        //显示模态框
+        $('#userModal').modal("show");
+
+        //获取用户id,jQuery 中提供了获取自定义属性的方法, data()
+        currentId = $(this).parent().data("id");
+
+        // 1 表示 已启用, 0 表示 已禁用
+        // 如果是禁用按钮，说明需要将该用户置成禁用状态，传 0
+        isDelete = $(this).hasClass("btn-danger") ? 0 : 1;
+    });
+
+    //3. 点击确认按钮，发送ajax请求，修改对应用户转态,需要两个参数(用户id，isDelete用户改成的转态)
+    $('#submitBtn').on("click", function() {
+        //发送 ajax
+        $.ajax({
+            type: "post",
+            url: "/user/updateUser",
+            data: {
+                id: currentId,
+                isDelete: isDelete
+            },
+            dataType: "json",
+            success: function (info) {
+                //1.关闭模态框
+                $('#userModal').modal("hide");
+                //2. 页面重新渲染
+                render();
+            }
+        })
+    })
 });
