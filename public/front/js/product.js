6e851134bf98268bbc5e40f4f5b439a25babb32e
TJS-95 <TJS_95@163.com>
2019-04-16 18:04:41 +0800

    商品详情页完成

---
+++ public/front/js/product.js
@@ -0,0 +1,78 @@
+$(function () {
+    // 功能1：获取地址栏的productId, 发送ajax请求, 进行商品渲染
+    var productId = getSearch("productId");
+
+    $.ajax({
+        type: "get",
+        url: "/product/queryProductDetail",
+        data: {
+            id: productId
+        },
+        dataType: "json",
+        success: function ( info ) {
+            //console.log(info)
+            var htmlStr = template("productTpl", info );
+            $('.mui-scroll').html(htmlStr);
+
+            // 手动进行轮播初始化
+            //获得slider插件对象
+            var gallery = mui('.mui-slider');
+            gallery.slider({
+                interval:3000//自动轮播周期，若为0则不自动播放，默认为0；
+            });
+
+            // 手动初始化数字框
+            mui('.mui-numbox').numbox();
+        }
+    });
+
+    // 让尺码可以选中
+    $('.lt_main').on("click", ".lt_size span", function () {
+        $(this).addClass("current").siblings().removeClass("current");
+    });
+
+    // 功能3：加入购物车功能
+    // (1) 添加点击事件
+    // (2) 手机尺码, 数量, 产品id, 发送ajax请求
+    $('#addCart').on("click", function() {
+        // 获取尺码, 数量
+        var size = $('.lt_size span.current').text();
+        if ( !size ) {
+            mui.toast("请选择尺码");
+            return;
+        }
+        // 获取数量
+        var num = $('.mui-numbox-input').val();
+
+        // 发送ajax请求
+        $.ajax({
+            type: "post",
+            url: "/cart/addCart",
+            data: {
+                productId: productId,
+                num: num,
+                size: size
+            },
+            dataType: "json",
+            success: function ( info ) {
+                // console.log( info )
+                // 加入购物车操作需要登录
+
+                // (1) 未登录情况
+                if ( info.error === 400 ) {
+                    // 跳转到登录页, 将来登录完成还要跳回来, 可以将当前页的地址传过去即可
+                    location.href = "login.html?retUrl=" + location.href;
+                }
+                // (2) 已登录
+                if ( info.success ) {
+                    mui.confirm("添加成功", " 温馨提示", ["去购物车", "继续浏览"], function ( e ) {
+                        if ( e.index === 0) {
+                            // 去购物车
+                            location.href = "cart.html";
+                        }
+                    });
+                }
+            }
+        });
+    });
+});
