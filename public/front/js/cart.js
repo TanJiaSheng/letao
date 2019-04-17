d054f7c712e63b2b0e463f6399f218e5308f35da
TJS-95 <TJS_95@163.com>
2019-04-17 11:48:54 +0800

    购物车-编辑功能完成

--- public/front/js/cart.js
+++ public/front/js/cart.js
@@ -45,24 +45,74 @@ $(function() {
     // (2) 获取在按钮中存储的id
     // (3) 发送ajax请求, 执行删除操作
     // (4) 页面重新渲染
-    $('.lt_main .mui-table-view').on("tap", ".btn_del", function () {
+    $('.lt_main').on("tap", ".btn_del", function () {
         var id = $(this).data("id");
-        // 发送 ajax 请求
-        $.ajax({
-            type: "get",
-            url: "/cart/deleteCart",
-            // 后台要传的 id 参数是一组数组格式
-            data: {
-                id: [id]
-            },
-            dataType: "json",
-            success: function ( info ) {
-                if ( info.success ) {
-                    // 删除成功
-                    mui('.mui-scroll-wrapper').pullRefresh().pulldownLoading();
-                }
+        mui.confirm("你确定要删除此商品吗？", "温馨提示", ["取消", "删除"], function ( e ) {
+            if ( e.index === 1) {
+                // 发送 ajax 请求
+                $.ajax({
+                    type: "get",
+                    url: "/cart/deleteCart",
+                    // 后台要传的 id 参数是一组数组格式
+                    data: {
+                        id: [id]
+                    },
+                    dataType: "json",
+                    success: function ( info ) {
+                        if ( info.success ) {
+                            // 删除成功
+                            mui('.mui-scroll-wrapper').pullRefresh().pulldownLoading();
+                        }
+                    }
+                });
             }
-        })
+        });
     });
 
+    // 4. 编辑功能
+    $('.lt_main').on("tap", ".btn_edit", function () {
+        // html5 里面有一个 dataset 可以一次获取所有的自定义属性
+        var obj = this.dataset;
+        var id = obj.id;
+        // 生成 htmlStr
+        var htmlStr = template("editTpl", obj );
+
+        // mui 将模板中 \n 换行标记, 解析成 <br> 标签, 就换行了
+        // 需要将模板中所有的 \n 去掉
+        htmlStr = htmlStr.replace( /\n/g, "" );
+
+        // 弹出对话框
+        // 确认框的内容, 支持传递 html
+        mui.confirm(htmlStr, "编辑商品", ["确认", "取消"], function ( e ) {
+            if ( e.index === 0) {
+                // 你点击的是确认按钮, 进行获取尺码, 数量, id进行 Ajax 提交
+                var size = $('.lt_size span.current').text(); // 尺码
+                var num = $('.mui-numbox-input').val(); // 数量
+                $.ajax({
+                    type: "post",
+                    url: "/cart/updateCart",
+                    data: {
+                        id: id,
+                        size: size,
+                        num: num
+                    },
+                    dataType: "json",
+                    success: function ( info ) {
+                        if ( info.success ) {
+                            // 下拉刷新一次
+                            mui('.mui-scroll-wrapper').pullRefresh().pulldownLoading();
+                        }
+                    }
+                });
+            }
+        });
+
+        // 进行数字狂初始化
+        mui('.mui-numbox').numbox();
+    });
+
+    // 5. 让尺码可选
+    $('body').on('click', ".lt_size span", function () {
+        $(this).addClass("current").siblings().removeClass("current");
+    });
 });
